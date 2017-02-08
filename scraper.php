dc7fbecadd110f113c9eeddd85ef9108c1f0a7a1
slav123 <slav123@gmail.com>
2017-02-08 10:51:46 +0100

    init commit

--- scraper.php
+++ scraper.php
@@ -1,21 +1,37 @@
-<?
+<?php
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
+
+
+$url = '';
+
+// Read in a page
+$html = scraperwiki::scrape('http://www.rba.gov.au/statistics/cash-rate/');
+
+// Find something on the page using css selectors
+$dom = new simple_html_dom();
+$dom->load($html);
+$ret = $dom->find('#datatable tr');
+foreach ($ret as $row)
+{
+	$effective_date = $row->find('th', 0)->plaintext;
+	$change         = $row->find('td', 0)->plaintext;
+	$cash_rate      = $row->find('td', 1)->plaintext;
+
+	if ($effective_date)
+	{
+		scraperwiki::save_sqlite(array('effective_date'), array(
+			'effective_date' => $effective_date,
+			'change'         => $change,
+			'cash_rate'      => $cash_rate
+		));
+	}
+}
+
+
 // // An arbitrary query against the database
 // scraperwiki::select("* from data where 'name'='peter'")
 
